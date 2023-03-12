90365b70ef6e7a00c91e5cd5ba811358afe8798d
vsdcu <vinit.saini2@mail.dcu.ie>
2023-03-12 14:30:30 +0000

    Code for base spark application using gcp mysql

---
+++ src/main/java/org/dcu/database/ConnectionManager.java
@@ -0,0 +1,29 @@
+package org.dcu.database;
+
+import java.util.Properties;
+
+public class ConnectionManager {
+
+    private String driverClass = "com.mysql.cj.jdbc.Driver";
+
+    private Properties props = new Properties();
+
+    private String url = "jdbc:mysql://35.193.69.26:3306/moralis";
+
+    public ConnectionManager() {
+        // define JDBC connection properties
+
+        props.setProperty("driver", driverClass);
+        props.setProperty("user", "root");
+        props.setProperty("password", "!!DCU_Cloud_SYS_2023!!");
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public Properties getProps() {
+        return props;
+    }
+
+}
