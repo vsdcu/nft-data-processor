4de4183510a1152a903508d362269aef52b78734
vsdcu <vinit.saini2@mail.dcu.ie>
2023-03-19 01:06:35 +0000

    My mac settings

--- src/main/java/org/dcu/processor/SparkMintersProcessor.java
+++ src/main/java/org/dcu/processor/SparkMintersProcessor.java
@@ -12,13 +12,22 @@ public class SparkMintersProcessor {
         SparkConf conf = new SparkConf()
                 .setAppName("Minters-Processor-Job")
                 .set("spark.app.id", "spark-minters-processor")
-                .set("spark.executor.instances", "4")
+                .set("spark.executor.instances", "6")
+                .set("spark.executor.cores", "4")
+                .set("spark.executor.memory", "10g")
+                .set("spark.default.parallelism", "24")
+                .set("spark.sql.shuffle.partitions", "128")
+                .set("spark.driver.maxResultSize", "2g");
+
+        //vsdcu: my mac setup
+/*              .set("spark.executor.instances", "4")
                 .set("spark.executor.cores", "4")
                 .set("spark.executor.memory", "6g")
                 .set("spark.default.parallelism", "24")
                 .set("spark.sql.shuffle.partitions", "128")
-                .set("spark.driver.maxResultSize", "1g");
+                .set("spark.driver.maxResultSize", "1g");*/
 
+        
         SparkSession spark = SparkSession.builder().config(conf).getOrCreate();
         System.out.println(">>>> Job to find the Minters metrics : " + spark);
 
